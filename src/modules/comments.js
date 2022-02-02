d9d23abe5f9da15d101f5d00abb962aa21570bdd
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-02 10:56:55 +0000

    Get comments on pop up load and populate comment section

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,4 +1,4 @@
-export default (id, user, comment) => {
+export default async (id, user, comment) => {
   fetch(
     'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/d0HqiZcQvtTYVZAFmqCY/comments',
     {
@@ -13,4 +13,23 @@ export default (id, user, comment) => {
       }),
     },
   );
+};
+
+const populateComments = (data) => {
+  const list = document.querySelector('.comment-list');
+  if (!data.length) {
+    list.innerHTML = '<p>No comments found</p>';
+  } else {
+    data.forEach((d) => {
+      list.innerHTML += `<p><small>${d.creation_date}</small> | <span>${d.username}:</span> ${d.comment}</p>`;
+    });
+  }
+};
+
+export const getComments = async (pokemon) => {
+  const request = await fetch(
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/d0HqiZcQvtTYVZAFmqCY/comments?item_id=${pokemon}`,
+  );
+  const dataText = await request.json();
+  populateComments(dataText);
 };
